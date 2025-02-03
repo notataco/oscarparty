dba669cac3e053d540f627af6ceeae47a8c26d19
notataco <edwardsw1327@aol.com>
2025-02-03 17:42:29 -0500

    added column

--- OscarsDB/Scripts/CreateUserPick.sql
+++ OscarsDB/Scripts/CreateUserPick.sql
@@ -3,6 +3,7 @@ GO
 
 CREATE TABLE [dbo].[UserPick] (
     [UserPickID] INT IDENTITY(1,1) NOT NULL,
+    [CategoryID] INT NOT NULL,
     [UserID] INT NOT NULL,
     [MovieID] INT NULL,
     [ActorID] INT NULL
