8b4fccd284c7e4ab2d8cc1944c682782172dcb38
Keely Mashburn <39414616+keelymashburn@users.noreply.github.com>
2025-02-05 20:40:17 -0500

    trying things

--- OscarSPA/src/app/app.routes.ts
+++ OscarSPA/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
+import { GraphComponent } from './home/graph/graph.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'new-entry', component: NewEntryComponent }
+    { path: 'new-entry', component: NewEntryComponent },
+    { path: 'graph', component: GraphComponent }
 ];
--- OscarSPA/src/app/home/graph/graph.component.ts
+++ OscarSPA/src/app/home/graph/graph.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-graph',
-  imports: [],
+  imports: [NgxChartsModule],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.scss'
 })
-export class GraphComponent {
-
+export class GraphComponent
+{
+  public data = [
+    { name: 'Keely', value: 6 },
+    { name: 'Will', value: 10 },
+    { name: 'Jake', value: 8 },
+  ];
 }
