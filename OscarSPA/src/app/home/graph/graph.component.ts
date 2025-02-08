b2264d4d7c2f639d81d391c8ae104234072038ed
Keely Mashburn <39414616+keelymashburn@users.noreply.github.com>
2025-02-08 12:13:24 -0500

    take 1 of chart

--- OscarSPA/src/app/home/graph/graph.component.ts
+++ OscarSPA/src/app/home/graph/graph.component.ts
@@ -1,17 +1,77 @@
 import { Component } from '@angular/core';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { jqxChartModule, jqxChartComponent } from 'jqwidgets-ng/jqxchart';
 
 @Component({
   selector: 'app-graph',
-  imports: [NgxChartsModule],
+  imports: [jqxChartModule],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.scss'
 })
 export class GraphComponent
 {
-  public data = [
-    { name: 'Keely', value: 6 },
-    { name: 'Will', value: 10 },
-    { name: 'Jake', value: 8 },
+  public sampleData = [
+    { Name: 'Keely', score: 6 },
+    { Name: 'Will', score: 10 },
+    { Name: 'Jake', score: 8 },
+    { Name: 'Miles', score: 1 },
+    { Name: 'Alexa', score: 4 },
+    { Name: 'Emily', score: 7 },
+    { Name: 'Katie', score: 2 },
+    { Name: 'Kaelyn', score: 6 },
   ];
+
+  padding: any = { left: 20, top: 5, right: 20, bottom: 40 };
+ 
+  titlePadding: any = { left: 90, top: 0, right: 0, bottom: 10 };
+
+  xAxis: any =
+  {
+    dataField: 'Name',
+    showGridLines: false,
+    labels: { angle: 60 }
+  };
+
+  valueAxis: any =
+  {
+      minValue: 0,
+      maxValue: 25,
+      tickMarks: {
+        visible: true,
+        interval: 1,
+        color: '#FFFFFF',
+      },
+      gridLines: {
+        visible: true,
+        interval: 1,
+        color: '#FFFFFF',
+      },
+      labels: { 
+        visible: true, 
+        color: '#FFFFFF',
+        formatFunction: (value: number) => {
+          return value;
+          }
+       }
+  };
+
+  seriesGroups: any[] =
+  [
+      {
+          type: 'column',
+          orientation: 'vertical',
+          columnsGapPercent: 50,
+          series: [
+              { dataField: 'score', displayText: 'Points' }
+          ]
+      }
+  ];
+
+  getWidth(): any 
+  {
+    if (document.body.offsetWidth) {
+        return '90%';
+    }
+
+    return 850;
+  }
 }
